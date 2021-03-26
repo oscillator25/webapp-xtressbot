930600644ce9f3d8a18b44408c49bbb33d6da39a
PRATYAKSH GUPTA <pratyaksh1615@gmail.com>
2021-03-26 12:14:26 +0530

    uploading build files

---
+++ build/precache-manifest.156d0bdc99ac7b7daa650b0dadfccd2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b52c66e61aa9bd9eb24f",
+    "url": "/static/css/main.dfa46577.chunk.css"
+  },
+  {
+    "revision": "b52c66e61aa9bd9eb24f",
+    "url": "/static/js/main.de2c2225.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9fd8a49abaf296a6846f",
+    "url": "/static/js/2.7571d261.chunk.js"
+  },
+  {
+    "revision": "89bc03562e9932397e952295ad81898f",
+    "url": "/index.html"
+  }
+];
